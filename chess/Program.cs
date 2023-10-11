ef1d35c3b7375e0acbd602189c961b734c9ff23b
Артем Кунченко <kunchenko911@ukr.net>
2023-10-12 00:36:58 +0300

    *

--- chess/Program.cs
+++ chess/Program.cs
@@ -12,9 +12,43 @@ namespace chess
         {
             const int row = 8;
             const int col = 8;
+            bool exit = true;
+            int choice;
             int[,] field = new int[row, col];
             int position_row, position_col;
             setArr(field, row, col);
+            while (exit)
+            {
+                printOptions();
+                choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 0:
+                        exit = false;
+                        Console.Clear();
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Invalid value");
+                        break;
+                }
+
+
+                Console.ReadKey();
+            }
+
             printField(field, row, col);
 
 
@@ -76,7 +110,24 @@ namespace chess
                 }
                 Console.WriteLine();
             }
-        } 
+        }
+        #endregion
+
+        #region PRINT OPTIONS
+        static void printOptions()
+        {
+            Console.Clear();
+            Console.WriteLine("Choose chess piece:");
+            Console.WriteLine("1 - KING");
+            Console.WriteLine("2 - QUEEN");
+            Console.WriteLine("3 - ROOK");
+            Console.WriteLine("4 - BISHOP");
+            Console.WriteLine("5 - KNIGHT");
+            Console.WriteLine("6 - PAWN");
+            Console.WriteLine("0 - for exit");
+            Console.WriteLine();
+
+        }
         #endregion
     }
 }
