ed7232c9a35950a1f58d79c6e4b07305a7e3b75e
Артем Кунченко <kunchenko911@ukr.net>
2023-10-13 00:59:59 +0300

    Were implemented functions of shapes placement

--- chess/Program.cs
+++ chess/Program.cs
@@ -12,15 +12,19 @@ namespace chess
         {
             const int row = 8;
             const int col = 8;
+            int pos_row = 0, pos_col = 0;
             bool exit = true;
             int choice;
             int[,] field = new int[row, col];
-            int position_row, position_col;
             setArr(field, row, col);
             while (exit)
             {
                 printOptions();
+                Console.Write("Number: ");
                 choice = int.Parse(Console.ReadLine());
+                Console.Clear();
+                printField(field, row, col);
+                
                 switch (choice)
                 {
                     case 0:
@@ -28,16 +32,40 @@ namespace chess
                         Console.Clear();
                         break;
                     case 1:
+                        setPosition(ref pos_row, ref pos_col);
+                        Console.Clear();
+                        showKing(field, pos_row, pos_col);
+                        printField(field, row, col);
                         break;
                     case 2:
+                        setPosition(ref pos_row, ref pos_col);
+                        Console.Clear();
+                        showQueen(field, pos_row, pos_col);
+                        printField(field, row, col);
                         break;
                     case 3:
+                        setPosition(ref pos_row, ref pos_col);
+                        Console.Clear();
+                        showRook(field, pos_row, pos_col);
+                        printField(field, row, col);
                         break;
                     case 4:
+                        setPosition(ref pos_row, ref pos_col);
+                        Console.Clear();
+                        showBishop(field, pos_row, pos_col);
+                        printField(field, row, col);
                         break;
                     case 5:
+                        setPosition(ref pos_row, ref pos_col);
+                        Console.Clear();
+                        showKnight(field, pos_row, pos_col);
+                        printField(field, row, col);
                         break;
                     case 6:
+                        setPosition(ref pos_row, ref pos_col);
+                        Console.Clear();
+                        showPawn(field, pos_row, pos_col);
+                        printField(field, row, col);
                         break;
                     default:
                         Console.Clear();
@@ -45,14 +73,10 @@ namespace chess
                         break;
                 }
 
-
+                setArr(field, row, col);
                 Console.ReadKey();
             }
-
-            printField(field, row, col);
-
-
-            Console.ReadKey();
+         
         }
         #region SET INITIAL ARRAY
         static void setArr(int[,] arr, int row, int col)
@@ -71,14 +95,14 @@ namespace chess
         #region PRINT FIELD
         static void printField(int[,] arr, int row, int col)
         {
-            int k = 8;
+            int k = 1;
             Console.Write(" ");
-            for (char i = 'A'; i < 'I'; i++)
+            for (int i = 1; i < 9; i++)
             {
                 Console.Write(" " + i);
             }
             Console.WriteLine();
-            for (int i = 0; i < row; i++, k--)
+            for (int i = 0; i < row; i++, k++)
             {
                 Console.Write(k + " ");
                 for (int j = 0; j < col; j++)
@@ -97,8 +121,8 @@ namespace chess
                     }
                     if (arr[i, j] == 2)
                     {
-                        Console.BackgroundColor = ConsoleColor.Green;
-                        Console.Write("  ");
+                        Console.BackgroundColor = ConsoleColor.DarkGreen;
+                        Console.Write("**");
                         Console.BackgroundColor = ConsoleColor.Black;
                     }
                     if (arr[i, j] == 3)
@@ -129,5 +153,100 @@ namespace chess
 
         }
         #endregion
+
+        #region SET POSITION
+        static void setPosition(ref int row, ref int col)
+        {
+            do
+            {
+                Console.WriteLine("Choose pocition (row and col) using numbers from 1 to 8");
+                Console.Write("Row: ");
+                row = int.Parse(Console.ReadLine());
+                Console.Write("Col: ");
+                col = int.Parse(Console.ReadLine());
+                if (row >= 1 && row <= 8 && col >= 1 && col <= 8) break;
+                Console.WriteLine("Invalid value");
+            }
+            while (row < 1 || row > 8 || col < 1 || col > 8);
+            row--;
+            col--;
+        }
+        #endregion
+
+        #region SHOW PAWN
+        static void showPawn(int[,] arr, int row, int col)
+        {
+            arr[row, col] = 2;
+            if ((row - 1) >= 0) arr[row - 1, col] = 3;
+            if ((row - 1) >= 0 && (col - 1) >= 0) arr[row - 1, col - 1] = 3;
+            if ((row - 1) >= 0 && (col + 1) <= 7) arr[row - 1, col + 1] = 3;
+        }
+        #endregion
+
+        #region SHOW KING
+        static void showKing(int[,] arr, int row, int col)
+        {
+            arr[row, col] = 2;
+            if ((row - 1) >= 0) arr[row - 1, col] = 3;
+            if ((row + 1) <= 7) arr[row + 1, col] = 3;
+            if ((col - 1) >= 0) arr[row, col - 1] = 3;
+            if ((col + 1) <= 7) arr[row, col + 1] = 3;
+        }
+        #endregion
+
+        #region SHOW QUEEN
+        static void showQueen(int[,] arr, int row, int col)
+        {
+            arr[row, col] = 2;
+            for (int i = -7; i < arr.Length; i++) //vertical placement
+            { if ((row + i) >= 0 && (row + i) <= 7 && (row + i) != row) arr[row + i, col] = 3; }
+            for (int i = -7; i < arr.Length; i++)//horisontal placement
+            { if ((col + i) >= 0 && (col + i) <= 7 && (col + i) != col) arr[row, col + i] = 3; }
+            for (int i = -7; i < arr.Length; i++)//diogonal placement
+            { if ((row + i) >= 0 && (row + i) <= 7 && (row + i) != row && (col + i) >= 0 && (col + i) <= 7 && (col + i) != col) arr[row + i, col + i] = 3; }
+            for (int i = -7; i < arr.Length; i++)//oposit diogonal placement
+            { if ((row + i) >= 0 && (row + i) <= 7 && (row + i) != row && (col - i) >= 0 && (col - i) <= 7 && (col - i) != col) arr[row + i, col - i] = 3; }
+
+        }
+        #endregion
+
+        #region SHOW ROOK
+        static void showRook(int[,] arr, int row, int col)
+        {
+            arr[row, col] = 2;
+            for (int i = -7; i < arr.Length; i++) //vertical placement
+            { if ((row + i) >= 0 && (row + i) <= 7 && (row + i) != row) arr[row + i, col] = 3; }
+            for (int i = -7; i < arr.Length; i++)//horisontal placement
+            { if ((col + i) >= 0 && (col + i) <= 7 && (col + i) != col) arr[row, col + i] = 3; }
+        }
+        #endregion
+
+        #region SHOW BISHOP
+        static void showBishop(int[,] arr, int row, int col)
+        {
+            arr[row, col] = 2;
+            for (int i = -7; i < arr.Length; i++)//diogonal placement
+            { if ((row + i) >= 0 && (row + i) <= 7 && (row + i) != row && (col + i) >= 0 && (col + i) <= 7 && (col + i) != col) arr[row + i, col + i] = 3; }
+            for (int i = -7; i < arr.Length; i++)//oposit diogonal placement
+            { if ((row + i) >= 0 && (row + i) <= 7 && (row + i) != row && (col - i) >= 0 && (col - i) <= 7 && (col - i) != col) arr[row + i, col - i] = 3; }
+
+        }
+        #endregion
+
+        #region SHOW KNIGHT
+        static void showKnight(int[,] arr, int row, int col)
+        {
+            arr[row, col] = 2;
+            if ((row - 2) >= 0 && (col - 1) >= 0) arr[row - 2, col - 1] = 3; //up-left
+            if ((row - 2) >= 0 && (col + 1) <= 7) arr[row - 2, col + 1] = 3; //up-right
+            if ((row + 2) <= 7 && (col - 1) >= 0) arr[row + 2, col - 1] = 3; //down-left
+            if ((row + 2) <= 7 && (col + 1) <= 7) arr[row + 2, col + 1] = 3; //down-right
+            if ((row - 1) >= 0 && (col - 2) >= 0) arr[row - 1, col - 2] = 3; //left-up
+            if ((row - 1) >= 0 && (col + 2) <= 7) arr[row - 1, col + 2] = 3; //right-up
+            if ((row + 1) <= 7 && (col - 2) >= 0) arr[row + 1, col - 2] = 3; //left-down
+            if ((row + 1) <= 7 && (col + 2) <= 7) arr[row + 1, col + 2] = 3; //right-down
+        } 
+        #endregion
+
     }
 }
